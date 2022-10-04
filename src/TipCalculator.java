60128bf6bf8fa192a650a6fbe813a79a504b2bc9
Alp Senay <alps2789@nycstudents.net>
2022-10-04 11:48:07 -0400

    working...

---
+++ src/TipCalculator.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class TipCalculator {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("How much is the total bill?");
+        double totalBill = scan.nextDouble();
+        String[] splitter = double.toString().split("\\.");
+    }
+}
